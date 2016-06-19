3af84c6f5bca671e6214fa843954a67143e4c209
Apollo Zhu <public-apollonian@outlook.com>
2016-06-19 01:58:54 -0400

    Init
    Here comes Xadow

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  AZOS
+//
+//  Created by Apollo Zhu on 6/19/16.
+//  Copyright © 2016 WWITDC. All rights reserved.
+//
+
+#include "mbed.h"
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+
+}
+
+int main()
+{
+    setup()
+    while (1) {
+        loop()
+    }
+}
